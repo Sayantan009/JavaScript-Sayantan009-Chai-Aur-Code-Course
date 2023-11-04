c83ce3dabb716b524e24d831e4d3d9c9c270630e
Sayantan <131001613+Sayantan009@users.noreply.github.com>
2023-11-04 12:46:03 +0000

    Operations

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -4,12 +4,12 @@ let score = "33"
 // let score = undefined
 // let score = true
 
-console.log(typeof(score));
+console.log(typeof (score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber));
-console.log(valueInNumber); 
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
 
 /*
  "33" = 33
@@ -25,7 +25,7 @@ let isLoggerIn = 1
 
 let boolIsLoggedIn = Boolean(isLoggerIn)
 
-console.log(typeof(boolIsLoggedIn));
+console.log(typeof (boolIsLoggedIn));
 console.log(boolIsLoggedIn);
 
 /*
@@ -39,5 +39,49 @@ let someNumber = 3123
 
 let srtNumber = String(someNumber)
 
-console.log(typeof(srtNumber));
+console.log(typeof (srtNumber));
 console.log(srtNumber);
+
+
+
+
+// ************************* Operations *************************
+
+let value = 3
+let negValue = -value
+
+// console.log(2+1);
+// console.log(2-1);
+// console.log(2*1);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(negValue);
+
+let str1 = "Hitesh"
+let str2 = "Hello"
+
+console.log(str1 + str2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2
+
+gameCounter = 100
+console.log(gameCounter);
+
+// Go to mdn js prefix and postfix operator
+
+gameCounter++
+console.log(gameCounter);
+
+++gameCounter
+console.log(gameCounter);
