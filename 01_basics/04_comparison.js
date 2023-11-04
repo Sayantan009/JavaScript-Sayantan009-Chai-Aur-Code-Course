d45d9089a2958a9d2843abe87bb7755f8cacdf74
Sayantan <131001613+Sayantan009@users.noreply.github.com>
2023-11-04 12:53:30 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// Make sure that you compare values of same datatype
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
+
+// ==, === and <,>,<=,>= works differently
+
+// console.log("2" == 2); // doesnt checks datatype
+// console.log("2" === 2); // checks datatype
