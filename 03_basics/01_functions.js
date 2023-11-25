fb3ca8be562c5f70972530f2a72ce6c40fbc1ead
Sayantan <131001613+Sayantan009@users.noreply.github.com>
2023-11-25 23:54:21 +0530

    more on functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -26,16 +26,53 @@ function addTwoNumbers(numOne, numTwo) {
 const result = addTwoNumbers(4, 354);
 // console.log(result);
 
-function userLoggedinMessage(username = "User#0009"){
+function userLoggedinMessage(username = "User#0009") {
     // if (username === undefined) {
     //     console.log("Enter a valid username!!");
     // }
     if (!undefined) {
         console.log("Enter a valid username!!");
     }
-    else{
+    else {
         return `${username} just logged in!!`
     }
 };
 
 console.log(userLoggedinMessage());
+
+
+
+
+// ******************************************************************************************************
+
+function calculateCartPrice(primary, secondary, ...num1) {
+    return num1
+}
+
+console.log(calculateCartPrice(112, 231, 123, 1342, 6969));
+
+
+// const userOne = {
+//     user_name: "Hitesh",
+//     price: 1231
+// } 
+
+function handleObject(anyObject) {
+    return (`Username is ${anyObject.user_name} and cart price is ${anyObject.price}`)
+}
+
+// console.log(handleObject(userOne));
+console.log(
+    handleObject({
+        user_name: "Hitesh",
+        price: 1231
+    }));
+
+
+const myArray = [200, 400, 600]
+
+function returnSecondValue(anyArray){
+    return (anyArray[1])
+}
+
+console.log(returnSecondValue(myArray));
