c63191ce896c08fd9b5c4dd8c922267cca83a419
Sayantan <131001613+Sayantan009@users.noreply.github.com>
2023-11-26 00:29:01 +0530

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,13 +9,52 @@ if (true) {
     var c = 470;
     d = 30;
 
-    console.log(`InSide Scope: ${a}`);
-    console.log(`InSide Scope: ${b}`);
-    console.log(`InSide Scope: ${c}`);
-    console.log(`InSide Scope: ${d}`);
+    // console.log(`InSide Scope: ${a}`);
+    // console.log(`InSide Scope: ${b}`);
+    // console.log(`InSide Scope: ${c}`);
+    // console.log(`InSide Scope: ${d}`);
 }
 
-console.log(`OutSide Scope: ${a}`);
-console.log(`OutSide Scope: ${b}`);
-console.log(`OutSide Scope: ${c}`);
-console.log(`OutSide Scope: ${d}`);
+// console.log(`OutSide Scope: ${a}`);
+// console.log(`OutSide Scope: ${b}`);
+// console.log(`OutSide Scope: ${c}`);
+// console.log(`OutSide Scope: ${d}`);
+
+// **********************************************************************************************************
+
+function one() {
+    const username = "Hitesh"
+    function two() {
+        console.log(username);
+        const website = "youtube.com"
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+
+if (true) {
+    let username = "Hitesh"
+    if (true) {
+        let website = " youtube"
+        console.log(`${username} ${website}`);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// ********************* Interesting ***********************
+console.log(addOne(21));
+function addOne(value) {
+    return value + 1;
+};
+
+
+// console.log(addTwo(32));
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(32));
