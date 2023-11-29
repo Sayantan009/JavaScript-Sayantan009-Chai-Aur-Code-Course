25c0bb9ce83eb642f677f427798a0cfaf2193add
Sayantan <131001613+Sayantan009@users.noreply.github.com>
2023-11-29 21:40:09 +0530

    while loop

---
+++ 05_iterators/02_whileLoop.js
@@ -0,0 +1,19 @@
+let i = 1
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+let myArr = ['Iron Man', 'Bat Man', 'Hit Man(Rohit)'];
+let itr = 0;
+
+while (itr < myArr.length) {
+    console.log(`The name of th e superHero is ${myArr[itr]}`);
+    itr++;
+}
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
