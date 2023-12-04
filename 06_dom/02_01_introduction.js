2e997f99099904f50e7777e36d36762976a2dab0
Sayantan <131001613+Sayantan009@users.noreply.github.com>
2023-12-04 22:02:17 +0530

    DOM

--- 06_dom/02_01_introduction.js
+++ 06_dom/02_01_introduction.js
@@ -1,1 +1,41 @@
-document.getElementById('title')="Chai aur code"
+// document.getElementById('title').className = "lol"
+// // document.getElementById('title').innerHTML = "Chai aur code"
+// document.getElementById('title').getAttribute('id')
+// document.getElementById('title').setAttribute('class', 'lol2 heading test')
+
+// const title = document.getElementById('title')
+// // title.innerHTML = "Logic"
+// title.style.color = 'red'
+// title.style.backgroundColor = 'black'
+// title.style.padding = '1rem'
+// title.style.margin = '1rem 0'
+// title.style.borderRadius = '1rem'
+
+// console.log(title.textContent)
+// console.log(title.innerHTML)
+// console.log(title.innerText)
+
+// console.log(document.querySelector('h1'))
+// console.log(document.querySelector('h2'))
+// console.log(document.querySelector("#title"))
+// console.log(document.querySelector(".heading"))
+// console.log(document.querySelector("#title:first-child"))
+// const myUl = document.querySelector("ul")
+// const turnGreen = myUl.querySelector('li')
+// turnGreen.style.backgroundColor = 'green'
+// turnGreen.innerText = "green"
+
+// let myLi = document.querySelectorAll('li')
+// myLi[2].style.backgroundColor = 'green'
+
+// const myH1 = document.querySelectorAll('h1')
+// myH1[0].style.backgroundColor = 'blue'
+
+// myLi.forEach((item)=>item.style.backgroundColor = 'black')
+
+const tempClassList = document.getElementsByClassName("list-item")
+
+const tempClassListArr = Array.from(tempClassList)
+console.log(tempClassListArr);
+tempClassListArr.map((item)=>item.style.color = 'red')
+tempClassListArr.forEach((item)=>item.style.color = 'orange')
